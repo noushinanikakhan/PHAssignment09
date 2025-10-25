ddec3f237f2e5e8fc8f3fdce170bffe549afed0a
Noushin Anika Khan <noushinanikakhan@gmail.com>
2025-10-25 21:49:18 +0600

    twentytwo

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -17,19 +17,19 @@ const Navbar = () => {
   }
       return (
       <div className="navbar shadow-sm w-11/12 mx-auto 
-      bg-primary text-[#1e3a8a]">
+      bg-primary text-[#1e3a8a] relative">
 
   <div className="navbar-start">
-    <div className="dropdown">
+    <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
       <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-     <li><Link to="/"> Home</Link></li>
-     <li><Link to="/details">Our Courses</Link></li>
-      <li><Link to="/myprofile" className="relative group">
+        tabIndex="0"
+        className="menu menu-sm dropdown-content bg-base-200 rounded-box z-50 mt-3 w-52 p-2 shadow text-blue-800">
+     <li className="text-blue-800"><Link to="/" className="text-blue-800"> Home</Link></li>
+     <li className="text-blue-800"><Link to="/details" className="text-blue-800">Our Courses</Link></li>
+      <li className="text-blue-800"><Link to="/myprofile" className="text-blue-800">
            My Profile </Link></li>
       </ul>
     </div>
